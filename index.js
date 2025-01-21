a1a96bd1ab774c8b6fb2b575d36ff3df89867f8e
Daycross <dev.sapucaia@gmail.com>
2025-01-21 17:04:51 -0300

    :sparkles: feat: add prompter

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
+const config = require('./config.js');
+const { prompt } = require('inquirer');
+
 module.exports = {
-    config: {
-      path: './config.js',
-    },
-  };
-  
+  prompter: (cz, commit) => {
+    const questions = [
+      {
+        type: 'list',
+        name: 'type',
+        message: config.messages.type,
+        choices: config.types,
+      },
+      {
+        type: 'list',
+        name: 'scope',
+        message: config.messages.scope,
+        choices: [
+          ...config.scopes.map(scope => scope.name),
+          new prompt.Separator(),
+          'custom',
+        ],
+      },
+      {
+        type: 'input',
+        name: 'customScope',
+        message: config.messages.customScope,
+        when: answers => answers.scope === 'custom',
+      },
+      {
+        type: 'input',
+        name: 'subject',
+        message: config.messages.subject,
+        validate: input => (input.length <= config.subjectLimit ? true : `Limite de ${config.subjectLimit} caracteres excedido.`),
+      },
+      {
+        type: 'input',
+        name: 'body',
+        message: config.messages.body,
+      },
+      {
+        type: 'input',
+        name: 'breaking',
+        message: config.messages.breaking,
+        when: answers => config.allowBreakingChanges.includes(answers.type),
+      },
+      {
+        type: 'input',
+        name: 'footer',
+        message: config.messages.footer,
+      },
+      {
+        type: 'confirm',
+        name: 'confirmCommit',
+        message: config.messages.confirmCommit,
+      },
+    ];
+
+    prompt(questions).then(answers => {
+      const scope = answers.customScope ? `(${answers.customScope})` : answers.scope ? `(${answers.scope})` : '';
+      const breaking = answers.breaking ? `\n\nBREAKING CHANGE:\n${answers.breaking}` : '';
+      const footer = answers.footer ? `\n\n${config.footerPrefix} ${answers.footer}` : '';
+
+      const commitMessage = `${answers.type}${scope}: ${answers.subject}${breaking}${footer}`;
+      if (answers.confirmCommit) {
+        commit(commitMessage);
+      }
+    });
+  },
+};
